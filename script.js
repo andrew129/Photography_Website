32d552b0fb7f5590ba010079c2e9bfbf8e35dbd0
andrew129 <ndrwstls544@gmail.com>
2021-10-12 22:49:37 -0400

    finished website

---
+++ script.js
@@ -0,0 +1,47 @@
+let imageArr = ["./images/sunset.JPG", "./images/sunset-2.jpg"];
+let currentIndex = 0;
+let imageTag = document.querySelector(".bio-img");
+let timer;
+document.querySelector(".scroll-btn").onclick = () => window.scrollTo(0, 0);
+window.addEventListener("scroll", function () {
+  if (parseInt(this.scrollY) === 0) {
+    document.querySelector(".scroll-btn").style.display = "none";
+  } else {
+    document.querySelector(".scroll-btn").style.display = "block";
+    document.querySelector(".scroll-btn").classList.add("scroll-fade");
+    setTimeout(
+      () =>
+        document.querySelector(".scroll-btn").classList.remove("scroll-fade"),
+      1000
+    );
+  }
+});
+
+function startShow() {
+  imageTag.src = imageArr[currentIndex];
+  imageTag.classList.remove("fade-out");
+  imageTag.classList.add("fade-in");
+
+  if (currentIndex === imageArr.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  setTimeout(() => {
+    imageTag.classList.remove("fade-in");
+    setTimeout(() => {
+      imageTag.classList.add("fade-out");
+      setTimeout(() => {
+        requestAnimationFrame(startShow);
+      }, 4000);
+    }, 4000);
+  }, 4000);
+}
+requestAnimationFrame(startShow);
+
+jQuery(function () {
+  jQuery(".toggle-menu a").click(function (e) {
+    e.preventDefault();
+    jQuery(".right-nav").toggleClass("toggleNav");
+  });
+});
