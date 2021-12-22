be0e1f040a9141c6d00a6bd1bf5a7f150b803a0b
andrew129 <ndrwstls544@gmail.com>
2021-12-22 00:59:51 -0500

    made seperate js file for image storage

---
+++ imageData.js
@@ -0,0 +1,74 @@
+let imageArr = ["./images/hannah1.jpeg", "./images/hannah2.jpeg"];
+
+let imgObj = {
+    Couples: ["./images/Chloe and Grayson Final Large 31.png",
+      "./images/Noelle _ Hayden Final Large 14.jpg", "./images/Nubia _ Angel Final Large 7.jpg", 
+      "./images/Sterling _ Jerred Final Edit large 6.jpg", "./images/Amy _ Alex Final Edit Large 13.jpg","./images/Amy _ Alex Final Large 3.jpg",
+      "./images/Amy _ Alex Final Large 19.jpg", "./images/Deja _ Jacob Final Edit Large 18.jpg",
+      "./images/Cassidy _ Hunter Final Large 6.jpg", 
+      './images/Chloe and Grayson Final Large 4.png',
+      './images/Ever _ Family Final Large 9.png',
+      './images/Ever _ Family Final Large 26.png',
+      "./images/Cassidy _ Hunter Final Large 12.jpg",
+      './images/Chloe and Grayson Final Large 16.png', 
+      "./images/Noelle _ Hayden Final Large 18.jpg",
+      './images/Nubia _ Angel Final Large 11.jpg', 
+      // './images/Sterling _ Jerred Final Edit large 12.jpg'
+    ],
+    Maternity: [
+      './images/Modjeska Final Large 6.png',
+      './images/Serena Final Edit Large 22.jpg', './images/Angela Large Final 12.jpg',
+      './images/Serena Final Edit Small 1.jpg', './images/Modjeska Final Large 4.png',
+      './images/Modjeska Final Large 7.png', './images/Noelle _ Ben Final Edit Large 1.jpg',
+      './images/Serena Final Edit Large 17.jpg'
+  
+  
+    ],
+    Families: [
+      './images/Kiersten _ Family Large 4 NO smile.jpg',
+      './images/Aliya _ Family Large 4 Final.jpg', './images/Ever _ Family Final Large 28.png',
+      './images/Jessica Family Final Large 17.png', './images/Ever _ Family Final Large 13.png',
+      './images/Ever _ Family Final Large 15.png', './images/Jessica Family Final Large 11.png',
+      './images/Kiersten _ Family Large 8.jpg', './images/Mara _ Family Final Large 12.jpg', 
+      './images/Mara _ Family Final Large 19.jpg', 
+  
+    
+  
+    ],
+    Children: [
+      './images/Aliya _ Family Final Small 10.jpg', './images/Jessica Family Final Large 2.png',
+      './images/Jessica Family Final Large 4.png', './images/Jessica Family Final Large 27.png', 
+      './images/Jessica Family Final Large 28.png', './images/Jessica Family Final Large 33.png',
+      './images/Modjeska Final Large 8.png', './images/Modjeska Final Large 9.png'
+  
+  
+  
+    ],
+    Graduation: [
+      './images/Chelsea Grad Final Large 7.jpg', './images/Chelsea Grad Final Large 9.jpg',
+      './images/Chelsea Grad Final Large 18.jpg',  
+    ]
+  }
+  
+  let typesOfPhotos = [
+    {
+      title: 'Couples',
+      image: './images/Chloe and Grayson Final Large 31.png'
+    },
+    {
+      title: 'Maternity',
+      image: './images/Modjeska Final Large 6.png'
+    },
+    {
+      title: 'Families',
+      image: './images/Kiersten _ Family Large 4 NO smile.jpg'
+    },
+    {
+      title: 'Children',
+      image: './images/Mara _ Family Final Large 19.jpg'
+    },
+    {
+      title: 'Graduation',
+      image: './images/Chelsea Grad Final Large 18.jpg'
+    }
+  ]
